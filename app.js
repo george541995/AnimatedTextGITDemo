c459784a99fc43ba1ce5618eb221283c2f854ad0
Your Name <george541995@gmail.com>
2020-12-21 17:29:10 -0600

    fwfwaf

--- app.js
+++
@@ -1,6 +0,0 @@
-const letters = document.querySelectorAll('.letter');
-const intervalid = setInterval(function()){
-    for (let letter of letters){
-        letter.style.color =randomRBG();
-    }
-}, 2000);
